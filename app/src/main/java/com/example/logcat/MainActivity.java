c7f450883d4807938b37fe69ada8a807fa88e09b
zenghuazhe <570421628@qq.com>
2016-09-06 10:28:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/logcat/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.logcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("LogCatActivity", "onCreate--------this is logcat--------execute");
+
+
+
+    }
+
+
+}
+
